d797d425db47097e0494066d935db0e785b15a9a
Ricardo Lima <47871470+ricardo2009@users.noreply.github.com>
2025-06-09 12:10:13 -0300

    feat: Update AI resources overview and add SKU distribution insights
    - Modified the AI resources overview to include specific subscription and workspace values.
    - Enhanced the dashboard with new visualizations for AI and ML resources, including conditional formatting and export options.
    - Added a new section for SKU distribution of AI services with a detailed query and visualization settings.
    - Improved the overall layout and accessibility of resource links in the dashboard.

---
+++ cloud/Untitled-1.rb
@@ -0,0 +1,16 @@
+
+
+Observações da referencia:
+
+Todas as tabelas desse workbook contem as seguintes configurações que devem ser aplicadas no nosso workbook:
+
+- No nome do recurso é colocado um link onde se o se o usuario clicar no nome do recurso ele já é direcionado para a página do recurso no portal do Azure. depois ele consegue volta para o dashboard
+  isso precisa ser aplicado em todas as tabelas do nosso workbook, sem excessão.
+- Formatação condicional em tabelas com valores de performance, utilizando "verde,amarelo,vermelho", sendo "verde=bom","amarelo=atenção","vermelho=risco ou alto pico de demanda"
+- Configuração de exportar para um excel, esse botão está disponivél em todas as tabelas.
+
+
+
+No workbook de referencia as seguintes abas dos recursos abaixo podem ter configurações que você pode copiar e aprimorar porque lá já está legal mais pode ter ainda mais completo:
+
+ E novamente com o link no nome e formatação condicional utilizando as cores "verde,amarelo,vermelho"
